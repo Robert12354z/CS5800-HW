5d808daf13a187f70abe31522a6feca7afe9e231
Robert Reyes <49956764+Robert12354z@users.noreply.github.com>
2024-03-16 11:55:17 -0700

    Update MacroAbstract.java

--- Homework4/MacroProblem/MacroAbstract.java
+++ Homework4/MacroProblem/MacroAbstract.java
@@ -2,9 +2,32 @@ package Homework4.MacroProblem;
 
 public class MacroAbstract {
 
-    private static MacroFactory instance;
+    private static MacroAbstract instance;
 
-    private final MacroFactory noRestrict;
-    
+    private final MacroFactory NoRestrict;  
+    private final MacroFactory Paleo;
+    private final MacroFactory NutAllergy;
+    private final MacroFactory Vegan;
+
+    private MacroAbstract(){
+        noRestrict = new NoRestrict();
+        paleo = new Paleo();
+        vegan = new Vegan();
+        nutAll = new NutAllergy();
+        
+    }
+
+    public static MacroAbstract getInstance() {
+        if (instance == null) {
+            instance = new MacroAbstract();
+        }
+        return instance;
+    }
+
+    public MacroFactory getFactory(DietPlan dietPlan){
+        return switch (dietPlan){
+            
+        }
+    }
 
 }
