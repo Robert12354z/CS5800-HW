f0f73b8b794c7ef8a6c76cc3f47b2aeee22ee242
Robert Reyes <49956764+Robert12354z@users.noreply.github.com>
2024-03-16 11:52:57 -0700

    portion of part 2

--- Homework4/MacroProblem/MacroFactory.java
+++ Homework4/MacroProblem/MacroFactory.java
@@ -12,172 +12,172 @@ public interface MacroFactory {
 }
 
     
-    class NoRestrict implements MacroFactory{
+class NoRestrict implements MacroFactory{
 
-        private static final Random ran = new Random();
+ private static final Random ran = new Random();
         
 
-        private static final ArrayList<Carbs> allCarbs = new ArrayList<>();
-        static{
-        allCarbs.add(new Bread());
-        allCarbs.add(new Lentils());
-        allCarbs.add(new Pistachio());
-        allCarbs.add(new Cheese());
-        }
-
-        int randomIndex = ran.nextInt(allCarbs.size());
-
-        private static final ArrayList<Protein> allProtein = new ArrayList<>();
-        static{
-        allProtein.add(new Fish());
-        allProtein.add(new Beef());
-        allProtein.add(new Chicken());
-        allProtein.add(new Tofu());
-        }
-
-        private static final ArrayList<Fats> allFats = new ArrayList<>();
-        static{
-        allFats.add(new Avocado());
-        allFats.add(new SourCream());
-        allFats.add(new Peanuts());
-        allFats.add(new Tuna());
-        }
-        @Override
-        public Carbs generateCarbs(){
-            return allCarbs.get(randomIndex);
-        } 
-        @Override
-        public Fats generateFats(){
-            return allFats.get(randomIndex);
-        }
-        @Override
-        public Protein generateProtein(){
-            return allProtein.get(randomIndex);
-        }
-
+ private static final ArrayList<Carbs> allCarbs = new ArrayList<>();
+    static{
+    allCarbs.add(new Bread());
+    allCarbs.add(new Lentils());
+    allCarbs.add(new Pistachio());
+    allCarbs.add(new Cheese());
     }
 
-    class Paleo implements MacroFactory{
+    int randomIndex = ran.nextInt(allCarbs.size());
 
-        private static final Random ran = new Random();
-        
-
-        private static final ArrayList<Carbs> allCarbs = new ArrayList<>();
-        static{
-        allCarbs.add(new Pistachio());
-        
-        }
-
-        int randomIndex = ran.nextInt(allCarbs.size());
-
-        private static final ArrayList<Protein> allProtein = new ArrayList<>();
-        static{
-        allProtein.add(new Fish());
-        allProtein.add(new Beef());
-        allProtein.add(new Chicken());
-        }
-
-        private static final ArrayList<Fats> allFats = new ArrayList<>();
-        static{
-        allFats.add(new Avocado());
-        allFats.add(new Peanuts());
-        allFats.add(new Tuna());
-        }
-        @Override
-        public Carbs generateCarbs(){
-            return allCarbs.get(ran.nextInt(allCarbs.size()));
-        } 
-        @Override
-        public Fats generateFats(){
-            return allFats.get(ran.nextInt(allFats.size()));
-        }
-        @Override
-        public Protein generateProtein(){
-            return allProtein.get(ran.nextInt(allProtein.size()));
-        }
-
+    private static final ArrayList<Protein> allProtein = new ArrayList<>();
+    static{
+    allProtein.add(new Fish());
+    allProtein.add(new Beef());
+    allProtein.add(new Chicken());
+    allProtein.add(new Tofu());
     }
 
-    class Vegan implements MacroFactory{
-
-        private static final Random ran = new Random();
-        
-
-        private static final ArrayList<Carbs> allCarbs = new ArrayList<>();
-        static{
-        allCarbs.add(new Pistachio());
-        allCarbs.add(new Bread());
-        allCarbs.add(new Lentils());
-
-        
-        }
-
-        int randomIndex = ran.nextInt(allCarbs.size());
-
-        private static final ArrayList<Protein> allProtein = new ArrayList<>();
-        static{
-        allProtein.add(new Tofu());
-        }
-
-        private static final ArrayList<Fats> allFats = new ArrayList<>();
-        static{
-        allFats.add(new Avocado());
-        allFats.add(new Peanuts());
-        }
-        @Override
-        public Carbs generateCarbs(){
-            return allCarbs.get(ran.nextInt(allCarbs.size()));
-        } 
-        @Override
-        public Fats generateFats(){
-            return allFats.get(ran.nextInt(allFats.size()));
-        }
-        @Override
-        public Protein generateProtein(){
-            return allProtein.get(ran.nextInt(allProtein.size()));
-        }
-
+    private static final ArrayList<Fats> allFats = new ArrayList<>();
+    static{
+    allFats.add(new Avocado());
+    allFats.add(new SourCream());
+    allFats.add(new Peanuts());
+    allFats.add(new Tuna());
+    }
+    @Override
+    public Carbs generateCarbs(){
+        return allCarbs.get(randomIndex);
+    } 
+    @Override
+    public Fats generateFats(){
+        return allFats.get(randomIndex);
+    }
+    @Override
+    public Protein generateProtein(){
+        return allProtein.get(randomIndex);
     }
 
-    class NutAllergy implements MacroFactory{
+}
 
-        private static final Random ran = new Random();
-        
+class Paleo implements MacroFactory{
 
-        private static final ArrayList<Carbs> allCarbs = new ArrayList<>();
-        static{
-        allCarbs.add(new Bread());
-        allCarbs.add(new Lentils());
-        allCarbs.add(new Cheese());
-        
-        }
+    private static final Random ran = new Random();
+    
 
-        int randomIndex = ran.nextInt(allCarbs.size());
+    private static final ArrayList<Carbs> allCarbs = new ArrayList<>();
+    static{
+    allCarbs.add(new Pistachio());
+    
+    }
 
-        private static final ArrayList<Protein> allProtein = new ArrayList<>();
-        static{
-        allProtein.add(new Fish());
-        allProtein.add(new Beef());
-        allProtein.add(new Chicken());
-        allProtein.add(new Tofu());
-        }
+    int randomIndex = ran.nextInt(allCarbs.size());
 
-        private static final ArrayList<Fats> allFats = new ArrayList<>();
-        static{
-        allFats.add(new Avocado());
-        allFats.add(new Tuna());
-        }
-        @Override
-        public Carbs generateCarbs(){
-            return allCarbs.get(ran.nextInt(allCarbs.size()));
-        } 
-        @Override
-        public Fats generateFats(){
-            return allFats.get(ran.nextInt(allFats.size()));
-        }
-        @Override
-        public Protein generateProtein(){
-            return allProtein.get(ran.nextInt(allProtein.size()));
+    private static final ArrayList<Protein> allProtein = new ArrayList<>();
+    static{
+    allProtein.add(new Fish());
+    allProtein.add(new Beef());
+    allProtein.add(new Chicken());
+    }
+
+    private static final ArrayList<Fats> allFats = new ArrayList<>();
+    static{
+    allFats.add(new Avocado());
+    allFats.add(new Peanuts());
+    allFats.add(new Tuna());
+    }
+    @Override
+    public Carbs generateCarbs(){
+        return allCarbs.get(ran.nextInt(allCarbs.size()));
+    } 
+    @Override
+    public Fats generateFats(){
+        return allFats.get(ran.nextInt(allFats.size()));
+    }
+    @Override
+    public Protein generateProtein(){
+        return allProtein.get(ran.nextInt(allProtein.size()));
+    }
+
+}
+
+class Vegan implements MacroFactory{
+
+    private static final Random ran = new Random();
+    
+
+    private static final ArrayList<Carbs> allCarbs = new ArrayList<>();
+    static{
+    allCarbs.add(new Pistachio());
+    allCarbs.add(new Bread());
+    allCarbs.add(new Lentils());
+
+    
+    }
+
+    int randomIndex = ran.nextInt(allCarbs.size());
+
+    private static final ArrayList<Protein> allProtein = new ArrayList<>();
+    static{
+    allProtein.add(new Tofu());
+    }
+
+    private static final ArrayList<Fats> allFats = new ArrayList<>();
+    static{
+    allFats.add(new Avocado());
+    allFats.add(new Peanuts());
+    }
+    @Override
+    public Carbs generateCarbs(){
+        return allCarbs.get(ran.nextInt(allCarbs.size()));
+    } 
+    @Override
+    public Fats generateFats(){
+        return allFats.get(ran.nextInt(allFats.size()));
+    }
+    @Override
+    public Protein generateProtein(){
+        return allProtein.get(ran.nextInt(allProtein.size()));
+    }
+
+}
+
+class NutAllergy implements MacroFactory{
+
+    private static final Random ran = new Random();
+    
+
+    private static final ArrayList<Carbs> allCarbs = new ArrayList<>();
+    static{
+    allCarbs.add(new Bread());
+    allCarbs.add(new Lentils());
+    allCarbs.add(new Cheese());
+    
+    }
+
+    int randomIndex = ran.nextInt(allCarbs.size());
+
+    private static final ArrayList<Protein> allProtein = new ArrayList<>();
+    static{
+    allProtein.add(new Fish());
+    allProtein.add(new Beef());
+    allProtein.add(new Chicken());
+    allProtein.add(new Tofu());
+    }
+
+    private static final ArrayList<Fats> allFats = new ArrayList<>();
+    static{
+    allFats.add(new Avocado());
+    allFats.add(new Tuna());
+    }
+    @Override
+    public Carbs generateCarbs(){
+        return allCarbs.get(ran.nextInt(allCarbs.size()));
+    } 
+    @Override
+    public Fats generateFats(){
+        return allFats.get(ran.nextInt(allFats.size()));
+    }
+    @Override
+    public Protein generateProtein(){
+        return allProtein.get(ran.nextInt(allProtein.size()));
         }
 
     }
