5019e39ef00dc056330b588adf6fead54c8eb6ba
Robert12354z <49956764+Robert12354z@users.noreply.github.com>
2024-03-27 11:37:02 -0700

    Update Main.java

--- Homework5/Decorator/Main.java
+++ Homework5/Decorator/Main.java
@@ -3,9 +3,12 @@ package Homework5.Decorator;
 public class Main {
 
     public static void main(String[] args) {
+        //ConcreteFood is a concrete component
         Food burger = new ConcreteFood(10.0);
         System.out.println("Base price: $" + burger.getPrice());
 
+
+        //Topping is a decorator
         burger = new Topping(burger, 2.0);
         System.out.println("Price after adding topping: $" + burger.getPrice());
 
@@ -13,11 +16,15 @@ public class Main {
         System.out.println("Price after adding another topping: $" + burger.getPrice());
 
         CustomerLoyalty loyalty = new CustomerLoyalty(0.15,false);
-
+        
+        
+        //Order is a decorator
         System.out.println("Loyalty status: " + loyalty.getLoyal());
         System.out.println("Loyalty discount: " + loyalty.getDiscount()*100 + "%");
         Order order = new Order(loyalty);
         order.addFood(burger);
+
+        //Prints the total price of the order
         System.out.println("Total price: $" + order.calculateTotal());
 
 
